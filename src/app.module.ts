e33367ed2755bb8b1b01cd48d0f3aa96af65d138
devid-coesard <truegrinde@gmail.com>
2023-10-13 14:47:59 +0300

    fix: sync fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { Weather } from './weather/weather.entity';
 
 const isProd = process.env.NODE_ENV === 'production';
 const HOST = isProd ? process.env.POSTGRES_HOST : 'localhost';
-const SYNC = !isProd;
 
 @Module({
     imports: [
@@ -21,7 +20,7 @@ const SYNC = !isProd;
             password: process.env.POSTGRES_PW,
             database: process.env.POSTGRES_DB,
             entities: [Weather],
-            synchronize: SYNC,
+            synchronize: true,
         }),
         WeatherModule,
     ],
