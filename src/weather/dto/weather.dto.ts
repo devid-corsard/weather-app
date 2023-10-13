5eb7bcb1602781ab208bf2057b9a9aa4a324cdb6
devid-coesard <truegrinde@gmail.com>
2023-10-13 14:16:37 +0300

    feat: added parameters validation

--- src/weather/dto/weather.dto.ts
+++ src/weather/dto/weather.dto.ts
@@ -1,5 +1,11 @@
+import { IsNumberString } from 'class-validator';
+
 export class WeatherDto {
+    @IsNumberString()
     readonly lat: number;
+
+    @IsNumberString()
     readonly lon: number;
+
     readonly part?: string;
 }
