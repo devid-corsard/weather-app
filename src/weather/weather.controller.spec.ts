57251ce989c184a3ce7ccf72861968028ffbb105
devid-coesard <truegrinde@gmail.com>
2023-10-13 11:11:34 +0300

    test: added controller test

--- src/weather/weather.controller.spec.ts
+++ src/weather/weather.controller.spec.ts
@@ -1,33 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { WeatherController } from './weather.controller';
 import { WeatherService } from './weather.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Weather } from './weather.entity';
+import { WeatherDto } from './dto/weather.dto';
+import { Part, Weather } from './weather.entity';
 
+const mockWeather: Weather = {
+    id: 1,
+    lon: 0,
+    lat: 0,
+    part: [],
+    data: {
+        current: {
+            sunrise: 1684926645,
+            sunset: 1684977332,
+            temp: 292.55,
+            feels_like: 292.87,
+            pressure: 1014,
+            humidity: 89,
+            uvi: 0.16,
+            wind_speed: 3.13,
+        },
+    },
+};
 describe('WeatherController', () => {
-    let controller: WeatherController;
+    let weatherController: WeatherController;
+    let weatherService: WeatherService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [TypeOrmModule.forFeature([Weather])],
             controllers: [WeatherController],
-            providers: [WeatherService],
+            providers: [
+                WeatherService,
+                {
+                    provide: WeatherService,
+                    useValue: {
+                        saveWeatherToDb: jest
+                            .fn()
+                            .mockResolvedValue('Succesfully saved'),
+                        getWeather: jest.fn().mockImplementation(
+                            (dto: WeatherDto): Promise<Weather> =>
+                                Promise.resolve({
+                                    ...mockWeather,
+                                    lon: dto.lon,
+                                    lat: dto.lat,
+                                    part: dto.part.split(',') as Part[],
+                                }),
+                        ),
+                    },
+                },
+            ],
         }).compile();
 
-        controller = module.get<WeatherController>(WeatherController);
+        weatherController = module.get<WeatherController>(WeatherController);
+        weatherService = module.get<WeatherService>(WeatherService);
     });
 
     it('should be defined', () => {
-        expect(controller).toBeDefined();
+        expect(weatherController).toBeDefined();
     });
 
-    it('should do some', () => {
-        expect(
-            controller.getWeather({
+    describe('getWeather()', () => {
+        it('Should get weather', () => {
+            const weatherDto = {
                 lat: 10.66,
                 lon: 13.55,
                 part: 'minutely,hourly',
-            }),
-        ).toBe('');
+            };
+            expect(weatherController.getWeather(weatherDto)).resolves.toEqual({
+                ...mockWeather,
+                lon: weatherDto.lon,
+                lat: weatherDto.lat,
+                part: weatherDto.part.split(','),
+            });
+        });
+    });
+
+    describe('saveWeather()', () => {
+        it('Should save weather', () => {
+            const weatherDto = {
+                lat: 10.66,
+                lon: 13.55,
+                part: 'minutely,hourly',
+            };
+            expect(weatherController.saveWeather(weatherDto)).resolves.toEqual(
+                'Succesfully saved',
+            );
+        });
     });
 });
